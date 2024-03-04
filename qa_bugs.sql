d85e8afefcf21d8cda1a63f9ce7e6ce414480322
Michael Fink <65927705+AnubisRooster@users.noreply.github.com>
2024-03-04 12:55:24 -0500

    Create qa_bugs.sql

---
+++ qa_bugs.sql
@@ -0,0 +1,21 @@
+SELECT 
+    CONCAT(YEAR(`jira_issues`.`created_at`), '-Q', 
+           QUARTER(`jira_issues`.`created_at`)) AS `Fiscal Quarter`,
+    COUNT(DISTINCT `jira_issues`.`id`) AS `Total Bugs`,
+    SUM(CASE WHEN `jira_issues`.`reporter` IN ('Inna Murashko', 'Yuliia Nedomirko') THEN 1 ELSE 0 END) AS `QA Bugs`,
+    ROUND((SUM(CASE WHEN `jira_issues`.`reporter` IN ('Inna Murashko', 'Yuliia Nedomirko') THEN 1 ELSE 0 END) / COUNT(DISTINCT `jira_issues`.`id`)) * 100, 2) AS `QA Percentage`
+FROM 
+    `jira_issues`
+WHERE 
+    (
+        (`jira_issues`.`project` = 'Platform')
+        OR (`jira_issues`.`project` = 'SingleStoreDB Cloud') 
+        OR (`jira_issues`.`project` = 'Web Development')
+    )
+    AND (`jira_issues`.`created_at` >= CONVERT_TZ('2021-01-01 00:00:00.000', 'UTC', @@session.time_zone)) 
+    AND (`jira_issues`.`created_at` < CONVERT_TZ('2024-01-01 00:00:00.000', 'UTC', @@session.time_zone))
+    AND (`jira_issues`.`issue_type` = 'Bug') -- Added condition for issue type
+GROUP BY 
+    CONCAT(YEAR(`jira_issues`.`created_at`), '-Q', QUARTER(`jira_issues`.`created_at`))
+ORDER BY 
+    `Fiscal Quarter` ASC;
